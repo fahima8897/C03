a53a74bca5cc795e7142878c01db3c2a723a7b3f
Fahima Boumellil <fahimaboumellil@MacBook-Air-de-Fahima.local>
2021-02-16 23:06:37 +0100

    a

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -1,17 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fboumell <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/16 15:46:01 by fboumell          #+#    #+#             */
-/*   Updated: 2021/02/16 15:55:36 by fboumell         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
 
-char	*ft_strncat(char *dest, char *src, unsigned int nb)
+char *ft_strncat(char *dest, char *src, unsigned int nb)
 {
+	unsigned int i;
+	unsigned int n;
 
+	i = 0;
+	n = 0;
+	while (dest[i])
+		i++;
+	while (src[n] && n < nb)
+	{
+		dest[i] = src[n];
+		i++;
+		n++;
+	}
+	dest[i] = '\0';
+	return (dest);	
+}
 
+int main()
+{
+	char src[] = "hello";
+	char dest[] = "sunshine";
+	printf("%s\n", ft_strncat(dest, src, 3));
+	return 0;
 }
