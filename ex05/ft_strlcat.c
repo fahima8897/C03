a792ba3ae36f8e0ec71ab1391cd53df1a02595b6
Fahima BOUMELLIL <fboumell@e3r7p2.42.fr>
2021-03-19 15:49:35 +0100

    a

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboumell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/19 14:57:29 by fboumell          #+#    #+#             */
+/*   Updated: 2021/03/19 15:16:13 by fboumell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < size)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (j + size);
+	k = 0;
+	while (src[k] != '\0')
+		k++;
+	return (k + size);
+}
+
